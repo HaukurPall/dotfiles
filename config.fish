40c2c38844256b8427265d46a3cbfa30e95c3fc2
Haukur <haukurpalljonsson@gmail.com>
2021-03-01 13:36:12 +0000

    Making the fish config check if programs are install before setting options.

--- config.fish
+++ config.fish
@@ -1,29 +1,43 @@
 # Disable welcome message
 set -Ux fish_greeting
 
-set -gx EDITOR vim
-
 # Use local installation before system.
 set -x PATH $HOME/.local/bin $PATH
 
-# Configure local installations usage in fish
-# fzf
-fzf_key_bindings
-set -gx FZF_DEFAULT_COMMAND "fd --type f"
-set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
+# If 'exa' is installed, use it instead of 'ls'
+if type -q exa
+    # exa > ls
+    alias ls='exa --classify'
+    alias ll='ls --header --long --git'
+    alias tree='ls --tree'
+end
+# If 'vim' is installed, use it instead of 'vi'
+if type -q vim
+    alias vi=vim
+    set -gx EDITOR vim
+end
+# If 'nvim' is installed, use it instead of 'vi' or 'vim'
+if type -q nvim
+    alias vi=nvim
+    alias vim=nvim
+    set -gx EDITOR nvim
+end
 
-# Powerline shell
+# If 'fzf' is installed, use it for history search in fish
+if type -q fzf
+    fzf_key_bindings
+    set -gx FZF_DEFAULT_COMMAND "fd --type f"
+    set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
+end
+
+# If powerline shell is installed, use it for the fish promt
 if type -q powerline-shell
     function fish_prompt
         powerline-shell --shell bare $status
     end
 end
 
-# exa > ls
-alias ls='exa --classify'
-alias ll='ls --header --long --git'
-
-# PATH
-set -x PATH $PATH ~/.poetry/bin
-# PyENV
-pyenv init - | source
+# If pyenv is installed, initialize it.
+if type -q pyenv
+    pyenv init - | source
+end
