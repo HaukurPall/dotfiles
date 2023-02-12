4e1305ed3207b6d5872946a195c5c9c206926fcb
Haukur Páll <haukurpalljonsson@gmail.com>
2023-02-12 22:28:48 +0000

    Even more fixes

--- fish/config.fish
+++ fish/config.fish
@@ -1,54 +1,68 @@
 #!/usr/bin/fish
 # Disable welcome message
-set -gx fish_greeting
+set -g fish_greeting
+# Set vi mode
+set -g fish_key_bindings fish_vi_key_bindings
+# Do not shorten the prompt path
 set -g fish_prompt_pwd_dir_length 0
-
-# Store the hostname - it's expensive to call
-set -l l_hostname (hostname)
-if test "$l_hostname" = manjaro
-    set -gx CONDA_HOME $HOME/.local/miniconda3
-    set -x PATH $HOME/.cargo/bin $PATH
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"^C
-
-else if test "$l_hostname" = birta
-    set -gx CONDA_HOME /data2/scratch/haukurpj/miniconda3
-
-else if test "$l_hostname" = ada 
-     or test "$l_hostname" = mideind-gpu-a100-temp
-    # CONDA_HOME is universally
-    set -gx HF_HOME /data/scratch/haukurpj/huggingface_cache
-    set -gx TRANSFORMERS_CACHE /data/scratch/haukurpj/transformers_cache
-    set -gx CUDA_VISIBLE_DEVICES "$CUDA_VISIBLE_DEVICES"
-    alias sq="squeue -O \"JobID:.6,Partition:.9,UserName:.9,State:.8 ,Name:35,NumCPUs:.8,tres-per-job:.10,tres-per-node:.10,TimeUsed:.12,TimeLeft:.12 ,Command:.20\" -S \"-S\""
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-else if test "$l_hostname" = maja
-    set -gx CONDA_HOME /data2/scratch/haukurpj/miniconda3
-
-else if test "$l_hostname" = pallas
-    set -gx CONDA_HOME /data2/scratch/haukurpj/miniconda3
-
-end
-
-for secret_file in $HOME/Secrets/*.fish
-    source $secret_file
-end
-
+# Pager - always use less
+set -gx PAGER less
 # Define the tool location
-set -gx TOOLS_DIR $HOME/Tools
+set -g TOOLS_DIR $HOME/Tools
 
 # Use local installation before system.
-set -x PATH $TOOLS_DIR/bin $PATH
-set -x PATH $HOME/.local/bin $PATH
+fish_add_path $TOOLS_DIR/bin
+fish_add_path $HOME/.local/bin
 
 # Useful aliases/functions
 # Make less nice
 alias less='less -S -N'
 alias tsvsplit="sed G | tr '\t' '\n'"
+alias sq="squeue -O \"JobID:.6,Partition:.9,UserName:.9,State:.8 ,Name:35,NumCPUs:.8,tres-per-job:.10,tres-per-node:.10,TimeUsed:.12,TimeLeft:.12 ,Command:.20\" -S \"-S\""
 
-# Aliases for installed programs
-source ~/.config/fish/link_programs.fish
+# Abbreviations
+# git
+abbr gaa 'git add --all'
+abbr gcm 'git commit -m'
+abbr gs 'git status'
+abbr gp 'git pull'
 
+# If 'bat' is installed
+if type -q bat
+    # use it as a man pager
+    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+    # define bathelp to parse help output
+    function bathelp --description 'Parse help output with bat'
+        bat --language=help --color=always --style=grid --line-range :500 --wrap=never --pager=never $argv
+    end
+end
+# If 'exa' is installed, use it instead of 'ls'
+if type -q exa
+    # exa > ls
+    alias ls='exa --classify'
+    alias ll='ls --header --long --git'
+    alias tree='ls --tree'
+end
+# If 'vim' is installed, use it instead of 'vi'
+if type -q vim
+    alias vi=vim
+    set -gx EDITOR vim
+end
+# If 'nvim' is installed, use it instead of 'vi' or 'vim'
+if type -q nvim
+    alias vi=nvim
+    alias vim=nvim
+    set -gx EDITOR nvim
+end
+# If the shell is started via vscode
+if test "$TERM_PROGRAM" = "vscode"
+    # set the editor to vscode
+    set -gx EDITOR code
+    # and source the vscode integration (still testing it)
+    source (code --locate-shell-integration-path fish)
+end
+
+# If conda is installed (at CONDA_HOME - universal variable)
 if test -n "$CONDA_HOME"
     set -gx CONDA_EXE "$CONDA_HOME/bin/conda"
     set _CONDA_ROOT "$CONDA_HOME"
@@ -59,4 +73,16 @@ if test -n "$CONDA_HOME"
     if not set -q CONDA_SHLVL
         set -gx CONDA_SHLVL 0
     end
+end
+
+# Store the hostname - it's expensive to call
+set -l l_hostname (hostname)
+if test "$l_hostname" = ada 
+     or test "$l_hostname" = mideind-gpu-a100-temp
+    # Be sure to set CUDA_VISIBLE_DEVICES on ada
+    set -gx CUDA_VISIBLE_DEVICES "$CUDA_VISIBLE_DEVICES"
+end
+
+for secret_file in $HOME/Secrets/*.fish
+    source $secret_file
 end
--- scripts/install_fzf.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-wget https://github.com/junegunn/fzf/releases/download/0.30.0/fzf-0.30.0-linux_amd64.tar.gz -O fzf.tar.gz
-tar -xf fzf.tar.gz
-cp fzf ~/.local/bin/
-wget https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.fish -O fish/functions/fzf_key_bindings.fish # Not versioned, will break
-rm fzf.tar.gz fzf
